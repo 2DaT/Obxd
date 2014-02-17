bb83d4c2a482ce02ecab72acc3db482d4948079c
2DaT <justdat@e1.ru>
2014-02-17 20:58:41 +0600

    GPL licensing

--- Source/Engine/Lfo.h
+++ Source/Engine/Lfo.h
@@ -1,3 +1,26 @@
+/*
+	==============================================================================
+	This file is part of Obxd synthesizer.
+
+	Copyright © 2013-2014 Filatov Vadim
+	
+	Contact author via email :
+	justdat_@_e1.ru
+
+	This file may be licensed under the terms of of the
+	GNU General Public License Version 2 (the ``GPL'').
+
+	Software distributed under the License is distributed
+	on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+	express or implied. See the GPL for the specific language
+	governing rights and limitations.
+
+	You should have received a copy of the GPL along with this
+	program. If not, go to http://www.gnu.org/licenses/gpl.html
+	or write to the Free Software Foundation, Inc.,  
+	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+	==============================================================================
+ */
 #pragma once
 #include "SynthEngine.h"
 class Lfo
@@ -29,7 +52,7 @@ public:
                 Res+=sq;
             if((waveForm&4)!=0)
                 Res+=sh;
-			return tptlpupw(s1, Res,200,SampleRateInv);
+			return tptlpupw(s1, Res,700,SampleRateInv);
 	}
 	void setSamlpeRate(float sr)
 	{
